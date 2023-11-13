9bc5d62c1456e3c1d3da3a526ca01c41af872c4c
andreyklaveren <andreyklaveren@gmail.com>
2023-11-13 14:31:18 -0300

    delete

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let jogador = 0
+let jogo_andamento = 1 // "1" indica que o jogo esta em andamento
+const tabuleiro = {0 : 0, 0, }
+let vitorias_x = 0
+let vitorias_y = 0
+
 function iniciarJogo() {
     for (let i = 1; i < 10; i++) {
         document.getElementById(i).innerHTML = "";
@@ -7,9 +13,21 @@ function iniciarJogo() {
 }
 
 function jogada(posicao) {
-    if (document.getElementById(posicao).innerHTML == "") {
-        document.getElementById(posicao).innerHTML = "X"
-    } else {
-        alert("Posição não disponível")
+    if (jogo_andamento == 1) {
+
+        if (document.getElementById(posicao).innerHTML == "") 
+        {
+
+            if (jogador == 0) 
+            {
+                document.getElementById(posicao).innerHTML = "X"
+                jogador = 1
+            } 
+            else 
+            {
+                document.getElementById(posicao).innerHTML = "O"
+                jogador = 0
+            }
+        }
     }
 }
