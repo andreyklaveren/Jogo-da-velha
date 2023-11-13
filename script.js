0fad956d6fdc74e310462ffcd92ddf2ef6728849
andreyklaveren <andreyklaveren@gmail.com>
2023-11-13 15:12:44 -0300

    js atualizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let jogador = 0
 let jogo_andamento = 1 // "1" indica que o jogo esta em andamento
-const tabuleiro = {0 : 0, 0, }
+var tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
 let vitorias_x = 0
 let vitorias_y = 0
 
@@ -22,12 +22,27 @@ function jogada(posicao) {
             {
                 document.getElementById(posicao).innerHTML = "X"
                 jogador = 1
+                tabuleiro(posicao) = "x"
             } 
             else 
             {
                 document.getElementById(posicao).innerHTML = "O"
                 jogador = 0
+                tabuleiro(posicao) = "o"
             }
-        }
+        }   
     }
 }
+
+
+
+
+
+function mostrar() {
+    for (let i = 0; i < 9; i++) {
+        document.getElementById("mostrar").innerHTML = tabuleiro;
+        
+    }
+} 
+
+mostrar();
