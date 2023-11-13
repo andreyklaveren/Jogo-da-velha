a62173dc05b2c2a5d378f9cf9b070b8f968fdd07
andreyklaveren <andreyklaveren@gmail.com>
2023-11-13 16:29:34 -0300

    script mais atualizado ainda

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ var tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
 let vitorias_x = 0
 let vitorias_y = 0
 
-function iniciarJogo() {
-    for (let i = 1; i < 10; i++) {
+function iniciarJogo() 
+{
+    for (let i = 1; i < 10; i++) 
+    {
         document.getElementById(i).innerHTML = "";
     }
 
     document.getElementById("botao").innerHTML = "Reiniciar jogo"
 }
 
-function jogada(posicao) {
-    if (jogo_andamento == 1) {
+function jogada(posicao) 
+{
+    if (jogo_andamento == 1) 
+    {
 
         if (document.getElementById(posicao).innerHTML == "") 
         {
@@ -22,27 +26,33 @@ function jogada(posicao) {
             {
                 document.getElementById(posicao).innerHTML = "X"
                 jogador = 1
-                tabuleiro(posicao) = "x"
-            } 
+                tabuleiro[posicao - 1] = "X"
+            }
             else 
             {
                 document.getElementById(posicao).innerHTML = "O"
                 jogador = 0
-                tabuleiro(posicao) = "o"
+                tabuleiro[posicao - 1] = "O"
             }
-        }   
+        }
+        VerificarVitoria()
+        mostrar();
     }
 }
 
 
-
-
+function VerificarVitoria() 
+{
+    // Verifica se há uma linha vencedora
+    for (let i = 0; i < 9; i += 3) 
+    {
+        if (tabuleiro[i] != 0 && tabuleiro[i] == tabuleiro[i + 1] && tabuleiro[i + 1] == tabuleiro[i + 2]) 
+        {
+            alert([jogador], "ganhou");
+        }
+    }
+}
 
 function mostrar() {
-    for (let i = 0; i < 9; i++) {
-        document.getElementById("mostrar").innerHTML = tabuleiro;
-        
-    }
+    document.getElementById("mostrar").innerHTML = tabuleiro;
 } 
-
-mostrar();
